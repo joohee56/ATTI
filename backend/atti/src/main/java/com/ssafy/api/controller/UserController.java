86a31db1dd9791d2ccb94d1978e32c41cad884cb
LeeJuhui <doohui96@naver.com>
2022-08-01 07:06:12 +0900

    [BE] [JH] create: 회원가입 기능

--- backend/atti/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/atti/src/main/java/com/ssafy/api/controller/UserController.java
@@ -1,5 +1,54 @@
 package com.ssafy.api.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.api.service.UserService;
+import com.ssafy.db.entity.user.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+	
+	private final UserService userService;
+	
+	// 단순 회원가입 페이지 이동
+	@GetMapping("/signup")	
+	public String register() {
+		return "user/join";
+	}
+	
+	// 단순 로그인 페이지 이동
+	@GetMapping("/login")	
+	public String login() {
+		return "user/login";
+	}
+	
+	// 일반 회원가입
+	@PostMapping("/signup/normal")
+	public String signUpNormal(@RequestBody User user) throws Exception {
+		// 유효성 체크
+		userService.signUp(user);
+		return "redirect:/user/login";
+	}
+	
+	// 소셜 회원가입
+	@PostMapping("/signup/social")
+	public void signUpSocial(User user) {
+		
+	}
+	
+	// 아이디 중복 체크
+	@GetMapping("/idcheck")
+	public void idCheck() {
+		
+	}
 
 }
