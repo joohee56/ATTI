56f6cb1fdea5ec577486dbea35184c0f161beb66
LeeHyeonT <gusxosmsdy@gmail.com>
2022-08-05 18:00:13 +0900

    [FE][HT]Merge

--- backend/atti/src/main/java/com/ssafy/api/controller/PostController.java
+++ backend/atti/src/main/java/com/ssafy/api/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ public class PostController {
 		return new ResponseEntity<List<Post>>(postService.viewAllPosts(), HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins="*")
 	@PostMapping("/write") // 게시글 쓰기
 	public ResponseEntity<String> createWriting(@RequestBody Post post) {
 		System.out.println(post);
