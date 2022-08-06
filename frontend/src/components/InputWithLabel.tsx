790afc3805159484207c77de7357457a71c278da
yeonsu-k <dustnzlzl@naver.com>
2022-08-07 03:27:24 +0900

    [FE][YS]Design(Input): 공통 input Design수정

--- frontend/src/components/InputWithLabel.tsx
+++ frontend/src/components/InputWithLabel.tsx
@@ -1,52 +1,50 @@
-import React from "react";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import { SvgIconComponent } from "@mui/icons-material";
+import SvgIcon from "@mui/material/SvgIcon";
+import { FormControl, InputAdornment } from "@mui/material";
 import styled from "styled-components";
+import { palette } from "../styles/palette";
 
 // 두개가 함께 있을땐 상단 (그 사이) 에 여백을 준다
 const Wrapper = styled.div`
   & + & {
-    margin-top: 0.2rem;
-  }
-`;
-
-const Label = styled.div`
-  font-size: 1rem;
-  color: #e9e9e9;
-  margin-bottom: 0.25rem;
-  text-align: left;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
-`;
-
-const Input = styled.input`
-  width: 50%;
-  border: 1px solid #bdbebd;
-  outline: none;
-  border-radius: 0px;
-  line-height: 2rem;
-  font-size: 0.8rem;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
-  border-radius: 0.4rem;
-  ::placeholder {
-    color: #bdbebd;
+    margin-top: 0.1rem;
   }
 `;
 
 interface inputInfo {
-  label?: string;
+  name: string;
+  placeholder: string;
+  icon?: SvgIconComponent;
   type?: string;
-  name?: string;
-  placeholder?: string;
   value?: string | number | readonly string[] | undefined;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>| undefined;
+  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
 }
 
+const InputStyle =styled(TextField)(({ theme }) => ({
+  backgroundColor: palette.gray_1,
+}));
+
 // rest 쪽에는 onChange, type, name, value, placeholder 등의 input 에서 사용 하는 값들을 넣어줄수 있다.
-const InputWithLabel = ({ label, ...rest }: inputInfo) => (
+const InputWithIcon = ({ icon,placeholder, ...rest }: inputInfo) => (
   <Wrapper>
-    {/* <Label>{label}</Label> */}
-    <Input {...rest}/>
+     <FormControl sx={{ m: 1, width: "54%" }}>
+        <InputStyle
+          InputProps={{
+            startAdornment: icon && (
+              <InputAdornment position="start">
+                <SvgIcon component={icon} inheritViewBox />
+              </InputAdornment>
+            )
+          }}
+          size="small"
+          placeholder={placeholder}
+          {...rest}
+        />
+        
+      </FormControl>
   </Wrapper>
 );
 
-export default InputWithLabel;
+export default InputWithIcon;
