c65d670c7333fb0b59ee633c2a0c5d9dbeb95817
yeonsu-k <dustnzlzl@naver.com>
2022-08-09 23:14:37 +0900

    [FE][YS]Create(axios) : Axios Instance 추가

--- frontend/src/utils/api/index.tsx
+++ frontend/src/utils/api/index.tsx
@@ -1,2 +1,20 @@
-const hyeontae = 1
-export default hyeontae
+import axios from "axios";
+import { BACKEND_URL } from "../../constant";
+
+export const api = axios.create({
+  baseURL: BACKEND_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+// 로그인 이후부터
+// export const apiAcc = axios.create({
+//   baseURL: BACKEND_URL,
+//   headers: {
+//     "Content-type": "application/json",
+//     "Authorization": ,
+//   },
+// });
+
+// export default apiAcc;
