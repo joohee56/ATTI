acbad0fec6c1809d04e46f2702748f833f7b690a
yeonsu-k <dustnzlzl@naver.com>
2022-08-14 05:08:43 +0900

    [FE][YS]Design[nav]:회원가입 추가

--- frontend/src/components/HeaderNav.tsx
+++ frontend/src/components/HeaderNav.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+
 import styled from "styled-components";
 import { ButtonBlue } from "./ButtonStyled";
 import LogoCirce from "../assets/images/logoCirce.png";
@@ -7,10 +7,19 @@ import AttiText4 from "../assets/images/Text/AttiText4.png";
 import { useDispatch, useSelector } from "react-redux";
 import { loginActions } from "../store/LoginSotre";
 import { RootState } from "../store";
-import { Avatar, Badge, IconButton } from "@mui/material";
+import {
+  Avatar,
+  Badge,
+  IconButton,
+  Link,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import { palette } from "../styles/palette";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function HeaderNav() {
   const { auth } = useSelector((state: RootState) => state.userInfo);
@@ -20,46 +29,118 @@ function HeaderNav() {
     dispatch(loginActions.logout());
   };
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div style={{ width: `100vw` }}>
+    <Main>
       <Header>
         <div style={{ display: `flex` }}>
-          <Link to={!`${auth}`?"/":"/community" }>
+          <Link href={`${!auth}` ? "/" : "/community"}>
             <LogoImg src={LogoCirce} alt="Logo Circle Img" />
           </Link>
           <LogoText src={AttiText4} alt="LogoText Img" />
         </div>
-        {!auth? (
-          <Link to="/login">
+        {!auth ? (
+          <div>
+          <Link href="/login" underline="none">
             <LoginButton>로그인</LoginButton>
           </Link>
+          <Link href="/signup" underline="none">
+          <LoginButton>회원가입</LoginButton>
+        </Link>
+        </div>
         ) : (
           // 쪽지 // 알림 // 프로필이미지
           <div style={{ display: `flex` }}>
-            <Badge color="secondary" badgeContent={0}>
+            {/* <Badge color="secondary" badgeContent={0}>
               <MailIcon />
             </Badge>
             <Badge color="secondary" badgeContent={0}>
               <NotificationsNoneIcon />
-            </Badge>
-            <IconButton>
-              <Avatar sx={{ width: 30, height: 30, bgcolor: palette.gray_2 }}>
-                PP
+            </Badge> */}
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+            >
+              <Avatar sx={{ width: 30, height: 30, bgcolor: palette.yellow_3 }}>
+                M
               </Avatar>
             </IconButton>
-            <LoginButton onClick={Logout}>로그아웃</LoginButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 25,
+                    height: 25,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem>
+                <Avatar /> My Profile
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon onClick={Logout}>
+                  <LogoutIcon fontSize="small" />
+                </ListItemIcon>
+                로그아웃
+              </MenuItem>
+            </Menu>
+            {/* <LoginButton onClick={Logout}>로그아웃</LoginButton> */}
           </div>
         )}
       </Header>
-    </div>
+    </Main>
   );
 }
+
+const Main = styled.div`
+  width: 100vw;
+  border-bottom: 1px solid ${palette.gray_2};
+`;
 
 const Header = styled.header`
   width: 90%;
   height: 30px;
   max-width: 1600px;
-  padding: 10px 3rem;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 5vw;
   font-weight: bold;
   display: flex;
   background-color: transparent;
@@ -83,12 +164,11 @@ const LogoText = styled.img`
   src={AttiText4} */
 `;
 
-const LoginButton = styled(ButtonBlue)`
+const LoginButton = styled.span`
   font-weight: bold;
   font-size: 0.875rem;
-  padding: 12px 24px;
+  padding: 10px 24px;
   border-radius: 8px;
 `;
-
 
 export default HeaderNav;
