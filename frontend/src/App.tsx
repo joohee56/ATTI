d8ce79800ce2fe7a7364b3d00b966bd5efa4eab0
yeonsu-k <dustnzlzl@naver.com>
2022-08-14 03:20:15 +0900

    [FE][YS]Feat(page): 로그인한 사용자만 커뮤페이지 이동 가능

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute.jsx"
 
 //import AddCommentIcon from "@mui/icons-material/AddComment";
 import Homepage from "./pages/Home";
@@ -11,14 +12,15 @@ import ModalTest from "./pages/ModalTest";
 import OAuth2RedirectHandler from "./components/account/OAuth2RedirectHandler";
 
 function App() {
+  const access=localStorage.getItem("AccessToken");
   return (
     <Routes>
     <Route path="/" element={ <Homepage />} />
     <Route path="/login" element={ <LoginPage />} />
     <Route path="/signup" element={ <SignupPage />} />
-    <Route path="/community" element={ <Community />} />
-    <Route path="/classmeeting" element={ <ClassMeeting />} />
-    <Route path="/welcome" element={ <WelcomePage />} />
+    <Route path="/community" element={<PrivateRoute  authenticated={access} component={<Community />}/>} />
+    <Route path="/classmeeting" element={<PrivateRoute  authenticated={access} component={<ClassMeeting />}/>} />
+    <Route path="/welcome" element={<PrivateRoute  authenticated={access} component={<WelcomePage />}/>} />
     <Route path="/modaltest" element={ <ModalTest />} />
     <Route path="/oauth/callback/kakao" element={<OAuth2RedirectHandler/>}/>
     </Routes>
