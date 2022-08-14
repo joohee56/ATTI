3bce821cda09722f3444e3c9831c38cea6ead3be
yeonsu-k <dustnzlzl@naver.com>
2022-08-14 18:01:52 +0900

    [FE][YS]Design(MyProfile): 프로필 이미지 수정 & 위치 수정

--- frontend/src/components/MyPage/MyInfo.tsx
+++ frontend/src/components/MyPage/MyInfo.tsx
@@ -8,6 +8,35 @@ import { ButtonBlue, ButtonPurple } from "../ButtonStyled";
 import InputWithLabel from "../InputWithLabel";
 import Modal from "../Modal";
 
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
 function MyPage() {
   // 모달 보이기 여부
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
@@ -31,122 +60,133 @@ function MyPage() {
   const [isPhoneNumber, setIsPhoneNumber] = useState<boolean>(false);
 
   return (
-    <>
-      <Main>
-        {!isEdit ? (
-          <>
-            <Avatar
-              sx={{
-                width: 100,
-                height: 100,
-                bgcolor: palette.gray_2,
-                marginBottom: 2,
-              }}
-            >
-              PP
+    <Main>
+      {!isEdit ? (
+        <>
+        <Content>
+        <Avatar
+            {...stringAvatar('Kent Dodds')} 
+            sx={{
+              width: 120,
+              height: 120,
+              bgcolor: palette.yellow_3,
+              marginBottom: 5,
+              fontSize: 40,
+            }}
+          >
             </Avatar>
-            <Grid>
-              <SpanStyle>Name</SpanStyle>
-              <InputWithLabel disabled name={""} placeholder={""} />
+          <Grid>
+            <SpanStyle>Name</SpanStyle>
+            <InputWithLabel disabled name={""} placeholder={""} />
 
-              <SpanStyle>ID</SpanStyle>
-              <InputWithLabel disabled name={""} placeholder={""} />
+            <SpanStyle>ID</SpanStyle>
+            <InputWithLabel disabled name={""} placeholder={""} />
 
-              <SpanStyle>Email</SpanStyle>
-              <InputWithLabel disabled name={""} placeholder={""} />
+            <SpanStyle>Email</SpanStyle>
+            <InputWithLabel disabled name={""} placeholder={""} />
 
-              <SpanStyle>Channel</SpanStyle>
-              <InputWithLabel disabled name={""} placeholder={""} />
-            </Grid>
-            <UserEdit onClick={() => setIsEdit(true)}>회원정보수정</UserEdit>
-            {isOpenModal && (
-              <Modal onClickToggleModal={() => setOpenModal(false)}>
-                <p>정말로 탈퇴하시곘습니까?</p>
-                <p>
-                  탈퇴시 관련 데이터들이 삭제 됩니다.
-                  <br />
-                  재가입시 데이터를 복구 할 수 없습니다.
-                </p>
-                <div>
-                  <button>아니오</button>
-                  <MemberWithdrawal>탈퇴</MemberWithdrawal>
-                </div>
-              </Modal>
-            )}
-             <MemberWithdrawal onClick={() => setOpenModal(true)}>
-              회원탈퇴
-            </MemberWithdrawal>
-          </>
-        ) : (
-          <>
-            <Avatar
-              sx={{
-                width: 100,
-                height: 100,
-                bgcolor: palette.gray_2,
-                marginBottom: 2,
-              }}
-            >
-              수정
-            </Avatar>
-            <StyledPage>
-              <StyledContent>
-                <Grid>
-                  <SpanStyle>Name</SpanStyle>
-                  <InputWithLabel name={""} placeholder={""} value={"이름"} />
+            <SpanStyle>Channel</SpanStyle>
+            <InputWithLabel disabled name={""} placeholder={""} />
+          </Grid>
+          <UserEdit onClick={() => setIsEdit(true)}>회원정보수정</UserEdit>
+          {isOpenModal && (
+            <Modal onClickToggleModal={() => setOpenModal(false)}>
+              <p>정말로 탈퇴하시곘습니까?</p>
+              <p>
+                탈퇴시 관련 데이터들이 삭제 됩니다.
+                <br />
+                재가입시 데이터를 복구 할 수 없습니다.
+              </p>
+              <div>
+                <button>아니오</button>
+                <MemberWithdrawal>탈퇴</MemberWithdrawal>
+              </div>
+            </Modal>
+          )}
+        </Content>
+         <MemberWithdrawal onClick={() => setOpenModal(true)}>
+         회원탈퇴
+       </MemberWithdrawal>
+       </>
+      ) : (
+        <Content>
+          <Avatar
+            sx={{
+              width: 100,
+              height: 100,
+              bgcolor: palette.gray_2,
+              marginBottom: 2,
+            }}
+          >
+            수정
+          </Avatar>
+          <StyledPage>
+            <StyledContent>
+              <Grid>
+                <SpanStyle>Name</SpanStyle>
+                <InputWithLabel name={""} placeholder={""} value={"이름"} />
 
-                  <SpanStyle>ID</SpanStyle>
-                  <InputWithLabel disabled name={""} placeholder={""} />
-                  <SpanStyle>Password</SpanStyle>
-                  <InputPassword
-                    name="password"
-                    placeholder="비밀번호"
-                    value={"비밀번호"}
-                    // onChange={onChangePassword}
-                  />
+                <SpanStyle>ID</SpanStyle>
+                <InputWithLabel disabled name={""} placeholder={""} />
+                <SpanStyle>Password</SpanStyle>
+                <InputPassword
+                  name="password"
+                  placeholder="비밀번호"
+                  value={"비밀번호"}
+                  // onChange={onChangePassword}
+                />
 
-                  <SpanStyle>Email</SpanStyle>
-                  <InputWithLabel
-                    name="password확인"
-                    placeholder="비밀번호 확인"
-                    type="password"
-                    value={""}
-                  />
-                </Grid>
-              </StyledContent>
-              <StyledContent>
-                <Grid>
-                  <SpanStyle>Email</SpanStyle>
-                  <InputWithLabel name={""} placeholder={""} value={"Email"} />
+                <SpanStyle>Email</SpanStyle>
+                <InputWithLabel
+                  name="password확인"
+                  placeholder="비밀번호 확인"
+                  type="password"
+                  value={""}
+                />
+              </Grid>
+            </StyledContent>
+            <StyledContent>
+              <Grid>
+                <SpanStyle>Email</SpanStyle>
+                <InputWithLabel name={""} placeholder={""} value={"Email"} />
 
-                  <SpanStyle>Phone</SpanStyle>
-                  <InputWithPhone
-                    name="phoneNumber"
-                    placeholder="폰 번호"
-                    phonNumber={phoneNumber}
-                    onChange={onChangePhonNumber}
-                    isCertifiedSuccess={setIsPhoneNumber}
-                  />
+                <SpanStyle>Phone</SpanStyle>
+                <InputWithPhone
+                  name="phoneNumber"
+                  placeholder="폰 번호"
+                  phonNumber={phoneNumber}
+                  onChange={onChangePhonNumber}
+                  isCertifiedSuccess={setIsPhoneNumber}
+                />
 
-                  <SpanStyle>Channel</SpanStyle>
-                  <InputWithLabel name={""} placeholder={""} />
-                </Grid>
-              </StyledContent>
-            </StyledPage>
-            <UserEdit onClick={InfoSubmit}>저장하기</UserEdit>
-          </>
-        )}
-      </Main>
-    </>
+                <SpanStyle>Channel</SpanStyle>
+                <InputWithLabel name={""} placeholder={""} />
+              </Grid>
+            </StyledContent>
+          </StyledPage>
+          <UserEdit onClick={InfoSubmit}>저장하기</UserEdit>
+        </Content>
+      )}
+    </Main>
   );
 }
 
 const Main = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  min-height: 70vh;
+  min-height: 80vh;
+  position: relative;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  left: 50%;
+  margin-top : 50px;
+  transform: translate(-50%);
 `;
 
 const Grid = styled.div`
@@ -156,6 +196,13 @@ const Grid = styled.div`
   row-gap: 20px;
   column-gap: 50px;
   align-items: stretch;
+`;
+
+const ProfileImg = styled(Avatar)`
+  width: 150;
+  height: 150;
+  background-color: ${palette.yellow_3};
+  margin-bottom: 6px;
 `;
 
 const SpanStyle = styled.span`
